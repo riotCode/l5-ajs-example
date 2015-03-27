0ce5a31d22a342330a14e4f48ef98bbc772eb9c5
riotCode <chasevecchio@gmail.com>
2015-03-27 07:45:59 -0400

    Updated basic structure and garbage cleaning

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,11 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', function()
+{
+    return view('home');
+});
 
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+Route::resource('item', 'ItemController',
+                ['only' => ['index', 'show', 'create']]
+);
